06028c396ee75d170ed2ef163d79cf5eba915169
hgleeee <94894328+hgleeee@users.noreply.github.com>
2022-07-17 11:40:54 +0900

    [Gold II] Title: 반도체 설계, Time: 284 ms, Memory: 21388 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n;
+    static int[] arr;
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = stoi(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[n+1];
+        for (int i = 1; i <= n; ++i) {
+            arr[i] = stoi(st.nextToken());
+        }
+
+        list.add(0);
+        for (int i = 1; i <= n; ++i) {
+            int len = list.size();
+            if (list.get(len-1) < arr[i]) {
+                list.add(arr[i]);
+            } else {
+                int idx = binarySearch(0, len-1, arr[i]);
+                list.set(idx, arr[i]);
+            }
+        }
+        System.out.println(list.size()-1);
+        br.close();
+    }
+
+    static int binarySearch(int lo, int hi, int val) {
+        if (lo == hi) {
+            return lo;
+        }
+        int mid = (lo+hi)/2;
+        if (list.get(mid) >= val) {
+            return binarySearch(lo, mid, val);
+        }
+        return binarySearch(mid+1, hi, val);
+    }
+
+    static int stoi(String input) {
+        return Integer.parseInt(input);
+    }
+
+}
