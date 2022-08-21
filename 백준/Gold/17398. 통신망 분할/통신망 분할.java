e8a80dc7ef34389860e4e6773a523b67de8dd003
hgleeee <94894328+hgleeee@users.noreply.github.com>
2022-08-22 00:36:13 +0900

    [Gold I] Title: 통신망 분할, Time: 480 ms, Memory: 47944 KB -BaekjoonHub

--- 백준/Gold/17398. 통신망 분할/통신망 분할.java
+++ 백준/Gold/17398. 통신망 분할/통신망 분할.java
@@ -3,78 +3,79 @@ import java.io.*;
 
 public class Main {
 
-    static class Connection {
-        int x, y;
-        public Connection(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    static int n, m, q;
-    static long[] size;
-    static int[] uf;
-    static boolean[] removeCon;
-    static Connection[] connections;
+    static int N, M, Q;
+    static int[][] arr;
+    static Stack<Integer> myQ = new Stack<>();
+    static int[] uf, size;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = stoi(st.nextToken());
-        m = stoi(st.nextToken());
-        q = stoi(st.nextToken());
-        connections = new Connection[m+1];
-        for (int i = 1; i <= m; ++i) {
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+        Q = stoi(st.nextToken());
+        arr = new int[M+1][2];
+        for (int i = 1; i <= M; ++i) {
             st = new StringTokenizer(br.readLine());
-            connections[i] = new Connection(stoi(st.nextToken()), stoi(st.nextToken()));
+            arr[i][0] = stoi(st.nextToken());
+            arr[i][1] = stoi(st.nextToken());
         }
 
-        size = new long[n+1];
-        uf = new int[n+1];
-        for (int i = 1; i <= n; ++i) {
-            size[i] = 1;
-            uf[i] = i;
-        }
-
-        removeCon = new boolean[m+1];
-        Stack<Integer> removeIdx = new Stack<>();
-        for (int i = 0; i < q; ++i) {
+        boolean[] connected = new boolean[M+1];
+        Arrays.fill(connected, true);
+        for (int i = 0; i < Q; ++i) {
             int a = stoi(br.readLine());
-            removeCon[a] = true;
-            removeIdx.push(a);
+            myQ.push(a);
+            connected[a] = false;
         }
 
-        for (int i = 1; i <= m; ++i) {
-            if (removeCon[i]) continue;
-            long xSize = size[find(connections[i].x)], ySize = size[find(connections[i].y)];
-            if (merge(connections[i].x, connections[i].y)) {
-                size[find(connections[i].x)] = xSize+ySize;
+        uf = new int[N+1];
+        size = new int[N+1];
+        for (int i = 1; i <= N; ++i) {
+            uf[i] = i;
+            size[i] = 1;
+        }
+
+        for (int i = 1; i <= M; ++i) {
+            if (connected[i]) {
+                int a = find(arr[i][0]), b = find(arr[i][1]);
+                if (union(arr[i][0], arr[i][1])) {
+                    if (a < b) {
+                        size[a] += size[b];
+                    } else {
+                        size[b] += size[a];
+                    }
+                }
             }
         }
 
         long ans = 0;
-        while (!removeIdx.isEmpty()) {
-            Integer remove = removeIdx.pop();
-            long xSize = size[find(connections[remove].x)], ySize = size[find(connections[remove].y)];
-            if (merge(connections[remove].x, connections[remove].y)) {
-                ans += xSize*ySize;
-                size[find(connections[remove].x)] = xSize+ySize;
+        while (!myQ.isEmpty()) {
+            Integer now = myQ.pop();
+            int a = find(arr[now][0]), b = find(arr[now][1]);
+            if (union(arr[now][0], arr[now][1])) {
+                ans += (long)size[a] * size[b];
+                if (a < b) {
+                    size[a] += size[b];
+                } else {
+                    size[b] += size[a];
+                }
             }
         }
         System.out.println(ans);
         br.close();
     }
 
-    static boolean merge(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a == b) {
+    static boolean union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
             return false;
         }
-        if (a < b) {
-            uf[b] = a;
+        if (x > y) {
+            uf[x] = y;
         } else {
-            uf[a] = b;
+            uf[y] = x;
         }
         return true;
     }
@@ -87,5 +88,6 @@ public class Main {
     static int stoi(String input) {
         return Integer.parseInt(input);
     }
+
 
 }
