d4ec6e01d86971f26efeaf69afd8e5e1b6c8b7cc
hgleeee <94894328+hgleeee@users.noreply.github.com>
2023-09-05 10:05:00 +0900

    [Gold V] Title: 색종이와 가위, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Gold/20444. 색종이와 가위/색종이와 가위.java
+++ 백준/Gold/20444. 색종이와 가위/색종이와 가위.java
@@ -3,25 +3,26 @@ import java.util.*;
 
 public class Main {
 
-    static long n, k;
+    static int n;
+    static long k;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Long.parseLong(st.nextToken());
+        n = stoi(st.nextToken());
         k = Long.parseLong(st.nextToken());
         System.out.println(binarySearch(0, n/2));
         br.close();
     }
 
-    static String binarySearch(long lo, long hi) {
+    static String binarySearch(int lo, int hi) {
         if (lo == hi) {
             if (getNumOfPieces(lo) == k) {
                 return "YES";
             }
             return "NO";
         }
-        long mid = (lo + hi) / 2;
+        int mid = (lo + hi) / 2;
         long ref = getNumOfPieces(mid);
         if (ref >= k) {
             return binarySearch(lo, mid);
@@ -29,7 +30,11 @@ public class Main {
         return binarySearch(mid+1, hi);
     }
 
-    static long getNumOfPieces(long x) {
-        return (1 + x) * (1 + n - x);
+    static long getNumOfPieces(int x) {
+        return (1 + x) * (1 + (long)n - x);
+    }
+
+    static int stoi(String input) {
+        return Integer.parseInt(input);
     }
 }
